d3aa61960634151235432564c1b6f2260d64479a
Andrew Hershberger <andrew.d.hershberger@gmail.com>
2014-02-19 21:14:56 -0500

    Initial Commit

---
+++ Fonts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fonts
+//
+//  Created by Andrew Hershberger on 2/19/14.
+//  Copyright (c) 2014 Two Toasters, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWTAppDelegate class]));
+    }
+}
